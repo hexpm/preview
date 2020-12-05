99347a31d6e7270095cd036aea7fe4b8be4f5d57
Todd Resudek <toddresudek@gmail.com>
2020-12-05 11:11:08 -0700

    Adds Logster

--- lib/preview_web/endpoint.ex
+++ lib/preview_web/endpoint.ex
@@ -39,6 +39,7 @@ defmodule PreviewWeb.Endpoint do
     cookie_key: "request_logger"
 
   plug Plug.RequestId
+  plug Logster.Plugs.Logger, excludes: [:params]
   plug Plug.Telemetry, event_prefix: [:phoenix, :endpoint]
 
   plug Plug.Parsers,
