c93d867753fc8f9930504ceaac37f8e0713be02a
Eric Meadows-Jönsson <eric.meadows.jonsson@gmail.com>
2021-01-22 12:22:08 +0000

    gzip assets

--- lib/preview_web/endpoint.ex
+++ lib/preview_web/endpoint.ex
@@ -23,7 +23,7 @@ defmodule PreviewWeb.Endpoint do
   plug Plug.Static,
     at: "/",
     from: :preview,
-    gzip: false,
+    gzip: true,
     only: ~w(css fonts images js favicon.ico robots.txt)
 
   # Code reloading can be explicitly enabled under the
