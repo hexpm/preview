7600270c8d89b4793f7c9e61c2414be7e2ed513f
Wojtek Mach <wojtek@wojtekmach.pl>
2021-04-10 10:25:01 +0200

    Update priv/seeds.exs

--- priv/seeds.exs
+++ priv/seeds.exs
@@ -1,23 +1,17 @@
 repo_bucket = Application.fetch_env!(:preview, :repo_bucket)
+
 {:ok, {200, _, data}} = :hex_repo.get_tarball(:hex_core.default_config(), "decimal", "2.0.0")
 Preview.Storage.put(repo_bucket, "tarballs/decimal-2.0.0.tar", data)
-
-message = %{
-  "Records" => [
-    %{
-      "eventName" => "ObjectCreated:Put",
-      "s3" => %{"object" => %{"key" => "tarballs/decimal-2.0.0.tar"}}
-    }
-  ]
-}
-
-ref = Broadway.test_message(Preview.Queue, Jason.encode!(message))
 
 {:ok, {200, _, data}} = :hex_repo.get_tarball(:hex_core.default_config(), "ecto", "0.2.0")
 Preview.Storage.put(repo_bucket, "tarballs/ecto-0.2.0.tar", data)
 
 message = %{
   "Records" => [
+    %{
+      "eventName" => "ObjectCreated:Put",
+      "s3" => %{"object" => %{"key" => "tarballs/decimal-2.0.0.tar"}}
+    },
     %{
       "eventName" => "ObjectCreated:Put",
       "s3" => %{"object" => %{"key" => "tarballs/ecto-0.2.0.tar"}}
@@ -28,7 +22,8 @@ message = %{
 ref = Broadway.test_message(Preview.Queue, Jason.encode!(message))
 
 receive do
-  {:ack, ^ref, [_], []} -> :ok
+  {:ack, ^ref, [_], []} ->
+    :ok
 after
   1000 ->
     raise "message timeout"
