8549f3652b0c406855de03d09c2df83267796504
Eric Meadows-Jönsson <eric.meadows.jonsson@gmail.com>
2023-05-03 15:16:39 +0200

    Multi purge

--- lib/preview/cdn/fastly.ex
+++ lib/preview/cdn/fastly.ex
@@ -1,13 +1,23 @@
 defmodule Preview.CDN.Fastly do
   @behaviour Preview.CDN
   @fastly_url "https://api.fastly.com/"
+  @fastly_purge_wait 4000
 
   def purge_key(service, keys) do
     keys = keys |> List.wrap() |> Enum.uniq()
     body = %{"surrogate_keys" => keys}
     service_id = Application.get_env(:preview, service)
+    sleep_time = div(Application.get_env(:preview, :fastly_purge_wait, @fastly_purge_wait), 2)
 
     {:ok, 200, _, _} = post("service/#{service_id}/purge", body)
+
+    Task.Supervisor.start_child(Preview.Tasks, fn ->
+      Process.sleep(sleep_time)
+      {:ok, 200, _, _} = post("service/#{service_id}/purge", body)
+      Process.sleep(sleep_time)
+      {:ok, 200, _, _} = post("service/#{service_id}/purge", body)
+    end)
+
     :ok
   end
 
