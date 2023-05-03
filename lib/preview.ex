248551b7783d0534b1879e8fbfa682bb50297654
Eric Meadows-Jönsson <eric.meadows.jonsson@gmail.com>
2023-05-03 15:16:27 +0200

    Increase timeout for batch tasks

--- lib/preview.ex
+++ lib/preview.ex
@@ -1,6 +1,8 @@
 defmodule Preview do
   require Logger
 
+  @timeout 20_000
+
   def process_object(key) do
     key
     |> build_message()
@@ -20,7 +22,7 @@ defmodule Preview do
   def process_all_sitemaps(paths) do
     paths
     |> Stream.map(&%{"preview:sitemap" => &1})
-    |> Task.async_stream(&send_message/1, max_concurrency: 10, ordered: false)
+    |> Task.async_stream(&send_message/1, max_concurrency: 10, ordered: false, timeout: @timeout)
     |> Stream.run()
   end
 
@@ -35,7 +37,7 @@ defmodule Preview do
         Preview.Bucket.update_latest_version(name, version)
       end,
       max_concurrency: 10,
-      ordered: false
+      ordered: false, timeout: @timeout
     )
     |> Stream.run()
   end
@@ -49,7 +51,7 @@ defmodule Preview do
   defp batched_send(keys) do
     keys
     |> Stream.map(&build_message/1)
-    |> Task.async_stream(&send_message/1, max_concurrency: 10, ordered: false)
+    |> Task.async_stream(&send_message/1, max_concurrency: 10, ordered: false, timeout: @timeout)
     |> Stream.run()
   end
 
