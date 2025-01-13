cc58de015ec4feab384195e6bb2afcb08d78fe4e
Eric Meadows-Jönsson <eric.meadows.jonsson@gmail.com>
2025-01-13 19:53:55 +0100

    Build docker images on github actions

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Preview.MixProject do
     [
       app: :preview,
       version: "0.1.0",
-      elixir: "~> 1.17",
+      elixir: "~> 1.18",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
